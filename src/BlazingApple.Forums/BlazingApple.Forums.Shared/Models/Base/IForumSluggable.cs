1aad908e3500806293e05b55222a3b29fa40b47e
taylorchasewhite <whitetc2@gmail.com>
2023-07-19 12:47:02 +0000

    Deploying to gh-pages from  @ b3399f912cf7b947d49dd99a4a797db544c50474 🚀

--- src/BlazingApple.Forums/BlazingApple.Forums.Shared/Models/Base/IForumSluggable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazingApple.Forums.Shared.Models.Base;
-
-/// <summary>A sluggable record, e.g. a record that has it's own navigable page.</summary>
-public interface IForumSluggable : IForumRecord
-{
-	/// <summary>The url segment for this entity.</summary>
-	string Slug { get; set; }
-}
