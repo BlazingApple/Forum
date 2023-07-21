a61cbffa852647ac996ffd1a946775e82cda0c44
Taylor White <whitetc2@gmail.com>
2023-07-21 09:04:04 -0400

    Update packages

--- src/BlazingApple.Forums/BlazingApple.Forums.Shared/Models/Posts/IPostComment.cs
+++ src/BlazingApple.Forums/BlazingApple.Forums.Shared/Models/Posts/IPostComment.cs
@@ -1,16 +1,11 @@
-﻿using BlazingApple.Forums.Shared.Models.Base;
+﻿using BlazingApple.Components.Shared.Interfaces;
+using BlazingApple.Forums.Shared.Models.Base;
 using BlazingApple.Forums.Shared.Models.Votes;
 
 namespace BlazingApple.Forums.Shared.Models.Posts;
 
-public partial interface IPostComment : IForumRecord
+public partial interface IPostComment : IForumRecord, IComment
 {
-	/// <summary>The user that authored this comment.</summary>
-	string UserId { get; set; }
-
-	/// <summary>The content of the comment.</summary>
-	string Content { get; set; }
-
 	/// <summary><see cref="IPost"/> associated with this comment.</summary>
 	IPost? Post { get; set; }
 
